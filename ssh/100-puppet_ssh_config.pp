5852ef19c7930a6534824c202832e1af5f01628d
Ryan Apreala <r.apreala@alustudent.com>
2023-03-24 17:55:58 +0000

    adv

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-
-# Define a puppet manifest file
-class ssh_config {
-
-  class ssh_config {
-  file_line { 'Turn off passwd auth':
-    path => '/etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no',
-  }
-
-  file_line { 'Declare identity file':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/school',
-    match => '^#?\s*IdentityFile',
-  }
+#config ssh using puppet
+include stdlib
+file_line { 'private keyphers':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school',
+}
+file_line { 'refuse authenticate using a password':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
 }
