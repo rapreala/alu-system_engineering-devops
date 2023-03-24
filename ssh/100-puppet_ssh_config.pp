f2719d463601f5a52b78b13f99890a6cdb94a49f
Ryan Apreala <r.apreala@alustudent.com>
2023-03-24 17:40:41 +0000

    adv.

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -3,21 +3,15 @@
 # Define a puppet manifest file
 class ssh_config {
 
-  # Define a file line resource that turns off password authentication in ssh config
+  class ssh_config {
   file_line { 'Turn off passwd auth':
-    path  => '/etc/ssh/sshd_config',
-    line  => 'PasswordAuthentication no',
-    match => '^#?PasswordAuthentication',
+    path => '/etc/ssh/ssh_config',
+    line => 'PasswordAuthentication no',
   }
 
-  # Define a file line resource that declares identity file in ssh config
   file_line { 'Declare identity file':
-    path  => '/etc/ssh/ssh_config',
-    line  => 'IdentityFile ~/.ssh/school',
-    match => '^#?IdentityFile',
+    path => '/etc/ssh/ssh_config',
+    line => 'IdentityFile ~/.ssh/school',
+    match => '^#?\s*IdentityFile',
   }
 }
-
-# Apply the ssh_config class to the node
-include ssh_config
-
